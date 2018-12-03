5bc64ea479653d88dca7927c17f8d9002ce80e37
王军 <jun.wang@langlive.com>
2018-12-03 19:04:33 +0800

    增加日程

--- calendar.js
+++ calendar.js
@@ -9,6 +9,11 @@ function wj_calendar(id, showHeader, showTail){
     this.month = (new Date()).getMonth()+1 //当前月份
     this.id = id //日历ID
     this.data = "" //日历字符串
+    this.top = []
+    this.middle = []
+    this.footer=[]
+    this.all=[]
+    this.index=[]
   }
 
   //二月天数
@@ -34,45 +39,60 @@ function wj_calendar(id, showHeader, showTail){
   wj_calendar.prototype.completionDayArray = function(year,month,type,days){
      var arr = []
      if(type == this.tail){
+        var next = this.next(year, month)
         for (var i = 1; i <= days; i++) {
+          this.footer.push(next.y+'-'+this.ii(next.m)+'-'+this.ii(i))
           arr.push(i)
         }
         return arr;
      }
      m = month -1 > 0 ? month-1 : 12
 
+     var last = this.last(year, month)
      if(this.in_array(m,this.thirtyOne)){
          for(var i = 32-days;i<=31;i++){
             arr.push(i)
+            this.top.push(last.y+'-'+this.ii(last.m)+'-'+this.ii(i))
          }
      }else if(this.in_array(m,this.thirty)){
          for(var i = 31-days;i<=30;i++){
             arr.push(i)
+            this.top.push(last.y+'-'+this.ii(last.m)+'-'+this.ii(i))
          }
      }else{
         var f = this.february(year)
         for(var i = f+1-days;i<=f;i++){
             arr.push(i)
+            this.top.push(last.y+'-'+this.ii(last.m)+'-'+this.ii(i))
         }
      }
      return arr;
   }
 
+  wj_calendar.prototype.ii = function(i){
+      if(parseInt(i)<10){
+         return "0"+i
+      }
+      return i
+  }
   //当前月的日期数组
   wj_calendar.prototype.monthDayArray = function(year,month){
      var arr = []
      if(this.in_array(month,this.thirtyOne)){
          for(var i = 1;i<=31;i++){
             arr.push(i)
+            this.middle.push(year+'-'+this.ii(month)+'-'+this.ii(i))
          }
      }else if(this.in_array(month,this.thirty)){
          for(var i = 1;i<=30;i++){
             arr.push(i)
+            this.middle.push(year+'-'+this.ii(month)+'-'+this.ii(i))
          }
      }else{
         var f = this.february(year)
         for(var i = 1;i<=f;i++){
             arr.push(i)
+            this.middle.push(year+'-'+this.ii(month)+'-'+this.ii(i))
         }
      }
      return arr;
@@ -101,6 +121,10 @@ function wj_calendar(id, showHeader, showTail){
   wj_calendar.prototype.getDayArray = function(year, month){
       year =  year || this.year;
       month = month || this.month;
+      this.top=[]
+      this.middle=[]
+      this.footer=[]
+      this.all=[]
       year = parseInt(year)
       month = parseInt(month)
       var a = this.completionDay(year,month)
@@ -126,12 +150,16 @@ function wj_calendar(id, showHeader, showTail){
       footer = check(footer,this.showTail)
 
       all = (top.concat(middle)).concat(footer)
+      this.all = (this.top.concat(this.middle)).concat(this.footer)
+           console.log(this.all);
       return all
   }
 
   //往模板中塞数据，模板自定义
-  wj_calendar.prototype.template = function(dayArray,func){
-      this.data = func(dayArray);
+  wj_calendar.prototype.template = function(dayArray,list,func){
+      this.containsAll(this.all, list)
+      this.data = func(dayArray,list);
+      console.log(this.index)
   }
 
   //渲染
@@ -140,48 +168,85 @@ function wj_calendar(id, showHeader, showTail){
   }
 
   //上个月
-  wj_calendar.prototype.lastMonth = function(func,nowYear,nowMonth){
+  wj_calendar.prototype.lastMonth = function(func,list,nowYear,nowMonth){
        nowYear = nowYear || this.year
        nowMonth = nowMonth || this.month
+       list = list || []
        var y=0,m=0
-       if(nowMonth == 1){
-          m = 12;
-          y = parseInt(nowYear) - 1;
-       }else{
-          y = parseInt(nowYear);
-          m = parseInt(nowMonth) - 1;
-       }
+       var last= this.last(nowYear, nowMonth)
+       y = last.y,m=last.m
        var dayArray = this.getDayArray(y,m)
-       this.template(dayArray,func)
+       this.template(dayArray,list,func)
        this.renderHtml()
        this.year = y
        this.month = m
   }
 
   //下个月
-  wj_calendar.prototype.nextMonth = function(func,nowYear, nowMonth){
+  wj_calendar.prototype.nextMonth = function(func,list,nowYear, nowMonth){
        nowYear = nowYear || this.year
        nowMonth = nowMonth || this.month
+       list = list || []
        var y=0,m=0
-       if(nowMonth == 12){
-          y =  parseInt(nowYear) + 1
-          m = 1
-       }else{
-          y = parseInt(nowYear)
-          m = parseInt(nowMonth) + 1
-       }
+       var next = this.next(nowYear, nowMonth)
+       y=next.y,m=next.m
+
        var dayArray = this.getDayArray(y,m)
-       this.template(dayArray,func)
+       this.template(dayArray,list,func)
        this.renderHtml()
        this.year = y
        this.month = m
   }
 
+   wj_calendar.prototype.contains = function(arrays, obj) {
+      var i = arrays.length;
+      while (i--) {
+          if (arrays[i] === obj) {
+            return i;
+          }
+      }
+      return null;
+  }
+
+  //取下标
+  wj_calendar.prototype.containsAll = function(arrays, objs){
+     for (var i = 0; i < objs.length; i++) {
+       this.index.push(this.contains(arrays,objs[i]))
+     }
+   }
+
+   //上个月日期
+   wj_calendar.prototype.last= function(nowYear, nowMonth){
+     var y=0,m=0
+     if(nowMonth == 1){
+          m = 12;
+          y = parseInt(nowYear) - 1;
+       }else{
+          y = parseInt(nowYear);
+          m = parseInt(nowMonth) - 1;
+       }
+     return {"y":y,"m":m}
+  }
+
+  //下个月日期
+  wj_calendar.prototype.next= function(nowYear, nowMonth){
+     var y=0,m=0
+     if(nowMonth == 12){
+          y =  parseInt(nowYear) + 1
+          m = 1
+     }else{
+         y = parseInt(nowYear)
+         m = parseInt(nowMonth) + 1
+      }
+     return {"y":y,"m":m}
+  }
+
   //当前月
-  wj_calendar.prototype.nowMonth = function(func,nowYear, nowMonth){
+  wj_calendar.prototype.nowMonth = function(func,list,nowYear, nowMonth){
        nowYear = nowYear || this.year
        nowMonth = nowMonth || this.month
+       list = list || []
        var dayArray = this.getDayArray(nowYear,nowMonth)
-       this.template(dayArray,func)
+       this.template(dayArray,list,func)
        this.renderHtml()
   }
